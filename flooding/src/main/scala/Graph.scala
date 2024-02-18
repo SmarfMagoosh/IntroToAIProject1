c648b60c6e203a19013d6f267f218f3f746db968
micahnico <micahnicodemus22@gmail.com>
2024-02-18 00:17:58 -0500

    hopefully write a working Graph.pick function

--- flooding/src/main/scala/Graph.scala
+++ flooding/src/main/scala/Graph.scala
@@ -1,27 +1,27 @@
 import scala.collection.mutable.ListBuffer as MutList
 import scala.collection.mutable.Queue as MutQueue
 import scala.annotation.tailrec
+import scala.collection.mutable.Map as MutMap
 
-/**
- * A standard undirected graph with labelled vertices
- *
- * @param vertices
- *    a range of integers indicating the number of vertices on the graph
- * @param edges
- *    ordered pairs of integers indicating undirected edges of the graph
- * @author
- *    Evan Dreher, Micah Nicodemus
- */
+/** A standard undirected graph with labelled vertices
+  *
+  * @param vertices
+  *   a range of integers indicating the number of vertices on the graph
+  * @param edges
+  *   ordered pairs of integers indicating undirected edges of the graph
+  * @author
+  *   Evan Dreher, Micah Nicodemus
+  */
 case class Graph(vertices: Range, edges: List[(Int, Int)]) {
-  /**
-   * Array to keep track of the labels for our graph, indices relate to vertices
-   */
+
+  /** Array to keep track of the labels for our graph, indices relate to
+    * vertices
+    */
   val labels: Array[Int] = vertices.map(_ => 0).toArray
 
-  /**
-   * Adjacency List for the Graph. Indices of the array correspond to vertices. Values in the list correspond
-   * to vertices it points to.
-   */
+  /** Adjacency List for the Graph. Indices of the array correspond to vertices.
+    * Values in the list correspond to vertices it points to.
+    */
   val adjacency: Array[List[Int]] = {
     val adjList: Array[MutList[Int]] = labels.map(_ => MutList())
     for edge <- edges do {
@@ -31,48 +31,39 @@ case class Graph(vertices: Range, edges: List[(Int, Int)]) {
     adjList.map(_.toList)
   }
 
-  /**
-   * TODO: insert description so our code is OP
-   *
-   * @param newColor
-   *   the color to change the vertex to
-   * @param vertex
-   *   the vertex to start the coloring from
-   * @return
-   *   a new Graph containing the resulting state after applying the operation
-   */
+  /** TODO: insert description so our code is OP
+    *
+    * @param newColor
+    *   the color to change the vertex to
+    * @param vertex
+    *   the vertex to start the coloring from
+    * @return
+    *   a new Graph containing the resulting state after applying the operation
+    */
   def pick(newColor: Int, vertex: Int = 0): Graph = {
-    val currentColor = labels(vertex)
     // don't do anything and return a copy if the color is the same
-    if currentColor == newColor then return this.copy()
+    if labels(vertex) == newColor then return this.copy()
 
-    // keep track of the vertices we've checked
-    val checked: Array[Boolean] = vertices.map(_ => false).toArray
-    checked(vertex) = true
+    val verticesToRemove =
+      adjacency(vertex).filter(v => labels(v) == newColor)
+    val verticesToRemain = vertices.diff(verticesToRemove).toList
+    val verticesMapping =
+      MutMap[Int, Int](verticesToRemain.zipWithIndex.map((v, i) => v -> i): _*)
+    for v <- verticesToRemove do verticesMapping += v -> verticesMapping(vertex)
 
-    @tailrec
-    def verticesToColor(
-        q: List[Int],
-        acc: List[Int] = List()
-    ): List[Int] = q match {
-      case Nil => acc
-      case h :: tail =>
-        if labels(h) == currentColor then
-          // getting newQ and updating checked are "constant time" bcz a vertex can have a max of 4 neighbors and we're prepending to tail
-          val newQ = adjacency(h).filter(v => !checked(v)) ::: tail
-          for v <- adjacency(h) do checked(v) = true
-          verticesToColor(newQ, h :: acc)
-        else verticesToColor(tail, acc)
-    }
-    val toColor = verticesToColor(List(vertex))
+    val newEdges = edges
+      .map((v1, v2) => {
+        val v1Mapped = verticesMapping(v1)
+        val v2Mapped = verticesMapping(v2)
+        (v1Mapped.min(v2Mapped), v1Mapped.max(v2Mapped))
+      })
+      .filter((v1, v2) => v1 != v2)
+      .distinct
 
-    // TODO: combine all vertices in the "blob"
-    // lmao
-
-    // return a new graph with the resulting state
-    val newGraph = this.copy()
-    for v <- toColor do newGraph.labels(v) = newColor
-    newGraph
+    val newG = Graph(0 until verticesToRemain.length, newEdges)
+    for v <- verticesToRemain do newG.labels(verticesMapping(v)) = labels(v)
+    newG.labels(verticesMapping(vertex)) = newColor
+    newG
   }
 
   override def toString: String =
