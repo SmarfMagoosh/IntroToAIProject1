00635b8bbc89f6158f805f03a399bb4b00e10052
Dreher <dreheren20@gcc.edu>
2024-02-17 21:18:48 -0500

    we do a little code cleanup

--- flooding/src/main/scala/Graph.scala
+++ flooding/src/main/scala/Graph.scala
@@ -2,15 +2,26 @@ import scala.collection.mutable.ListBuffer as MutList
 import scala.collection.mutable.Queue as MutQueue
 import scala.annotation.tailrec
 
-/** A standard undirected graph with labellable vertices
-  * @param vertices a range of integers indicating
-  * @param edges ordered pairs of integers indicating undirected edges of the graph
-  */
+/**
+ * A standard undirected graph with labelled vertices
+ *
+ * @param vertices
+ *    a range of integers indicating the number of vertices on the graph
+ * @param edges
+ *    ordered pairs of integers indicating undirected edges of the graph
+ * @author
+ *    Evan Dreher, Micah Nicodemus
+ */
 case class Graph(vertices: Range, edges: List[(Int, Int)]) {
-  // current color labels of the graph
+  /**
+   * Array to keep track of the labels for our graph, indices relate to vertices
+   */
   val labels: Array[Int] = vertices.map(_ => 0).toArray
 
-  // adjacency list of the graph (array used for constant lookups)
+  /**
+   * Adjacency List for the Graph. Indices of the array correspond to vertices. Values in the list correspond
+   * to vertices it points to.
+   */
   val adjacency: Array[List[Int]] = {
     val adjList: Array[MutList[Int]] = labels.map(_ => MutList())
     for edge <- edges do {
@@ -20,13 +31,16 @@ case class Graph(vertices: Range, edges: List[(Int, Int)]) {
     adjList.map(_.toList)
   }
 
-  /** @param newColor
-    *   the color to change the vertex to
-    * @param vertex
-    *   the vertex to start the coloring from
-    * @return
-    *   a new Graph containing the resulting state after applying the operation
-    */
+  /**
+   * TODO: insert description so our code is OP
+   *
+   * @param newColor
+   *   the color to change the vertex to
+   * @param vertex
+   *   the vertex to start the coloring from
+   * @return
+   *   a new Graph containing the resulting state after applying the operation
+   */
   def pick(newColor: Int, vertex: Int = 0): Graph = {
     val currentColor = labels(vertex)
     // don't do anything and return a copy if the color is the same
@@ -53,6 +67,7 @@ case class Graph(vertices: Range, edges: List[(Int, Int)]) {
     val toColor = verticesToColor(List(vertex))
 
     // TODO: combine all vertices in the "blob"
+    // lmao
 
     // return a new graph with the resulting state
     val newGraph = this.copy()
