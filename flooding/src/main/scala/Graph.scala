a6647d0c1f6b0df6967dedd0c74ee7f993662160
micahnico <micahnicodemus22@gmail.com>
2024-03-17 19:07:41 -0400

    update graph tostring

--- flooding/src/main/scala/Graph.scala
+++ flooding/src/main/scala/Graph.scala
@@ -2,21 +2,22 @@ import scala.collection.mutable.ListBuffer as MutList
 import scala.annotation.tailrec
 
 /** A standard undirected graph with labelled vertices
- *
- * @param vertices a range of integers indicating the number of vertices on the graph
- * @param edges    ordered pairs of integers indicating undirected edges of the graph
- * @author Evan Dreher, Micah Nicodemus
- */
+  *
+  * @param vertices
+  *   a range of integers indicating the number of vertices on the graph
+  * @param edges
+  *   ordered pairs of integers indicating undirected edges of the graph
+  * @author
+  *   Evan Dreher, Micah Nicodemus
+  */
 case class Graph(vertices: Range, edges: List[(Int, Int)]) {
 
-  /**
-   * Array to keep track of the labels of the vertices
-   */
+  /** Array to keep track of the labels of the vertices
+    */
   val labels: Array[Int] = vertices.map(_ => 0).toArray
 
-  /**
-   * Adjacency List for the Graph
-   */
+  /** Adjacency List for the Graph
+    */
   val adjacency: Array[List[Int]] = {
     val adjList: Array[MutList[Int]] = labels.map(_ => MutList())
     for edge <- edges do {
@@ -26,18 +27,20 @@ case class Graph(vertices: Range, edges: List[(Int, Int)]) {
     adjList.map(_.toList)
   }
 
-  /**
-   * true if this graph is a goal state (has only 1 vertex)
-   */
+  /** true if this graph is a goal state (has only 1 vertex)
+    */
   val isGoal: Boolean = this.vertices.end == 1
 
   /** Recolors a vertex to be a new color and blobifies the resulting graph so
-   * that no neighbors have the same color
-   *
-   * @param newColor the color to change the vertex to
-   * @return a new Graph containing the resulting state after applying the operation
-   * @author Micah Nicodemus
-   */
+    * that no neighbors have the same color
+    *
+    * @param newColor
+    *   the color to change the vertex to
+    * @return
+    *   a new Graph containing the resulting state after applying the operation
+    * @author
+    *   Micah Nicodemus
+    */
   def pick(newColor: Int): Graph = {
     // don't do anything and return a copy if the color is the same
     if labels.head == newColor then return Graph(vertices, edges)
@@ -68,5 +71,7 @@ case class Graph(vertices: Range, edges: List[(Int, Int)]) {
   }
 
   override def toString: String =
-    s"${vertices.map(v => s"$v (${labels(v)}) -> ${adjacency(v).mkString("[", ", ", "]")}").mkString("\n")}"
+    s"Vertex (Color) -> Neighbors\n${vertices
+        .map(v => s"$v (${labels(v)}) -> ${adjacency(v).mkString("[", ", ", "]")}")
+        .mkString("\n")}"
 }
