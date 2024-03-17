2648c3732c052727cea5653de54944971d0137ad
micahnico <micahnicodemus22@gmail.com>
2024-03-17 19:45:12 -0400

    more formatting

--- flooding/src/main/scala/Main.scala
+++ flooding/src/main/scala/Main.scala
@@ -1,7 +1,15 @@
-@main def main(size: Int, colors: Int, id: Int, heuristic: Int, algorithm: Int, verbose: Int): Unit = {
+@main def main(
+    size: Int,
+    colors: Int,
+    id: Int,
+    heuristic: Int,
+    algorithm: Int,
+    verbose: Int
+): Unit = {
   val r = run_experiment(size, colors, id, heuristic, algorithm, verbose)
-  println(s"\n\nSolution found: ${r.solution}")
+  println(s"\nSolution found: ${r.solution.mkString(" -> ")}\n")
   val g = get_experiment(size, colors, id)
-  println(s"Graph before actions: $g")
+  println(s"Graph before actions: \n$g\n")
   println(s"Proof of solution: ${r.solution.foldLeft(g)(_ pick _)}")
 }
+
